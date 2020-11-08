09fa3c22242ce40947b8d86f644539e0e767685d
GoldenEastLight <goldeneastlight@gmail.com>
2020-11-08 18:35:14 +0900

    admin page 시작

---
+++ HappyHouse_Admin/src/main/java/com/ssafy/happyhouse/HappyHouseAdminApplication.java
@@ -0,0 +1,12 @@
+package com.ssafy.happyhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyHouseAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyHouseAdminApplication.class, args);
+	}
+}
