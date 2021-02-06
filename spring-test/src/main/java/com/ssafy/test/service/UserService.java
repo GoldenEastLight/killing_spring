e772729d7d90bfcdc4dd6a4ed402761583ed9759
DESKTOP-KVCQHCD\multicampus <goldeneastlight@gmail.com>
2021-02-06 23:25:08 +0900

    kakaologin 스프링에서 구현 코드 백업 추가

--- spring-test/src/main/java/com/ssafy/test/service/UserService.java
+++ spring-test/src/main/java/com/ssafy/test/service/UserService.java
@@ -1,9 +1,14 @@
 package com.ssafy.test.service;
 
+import com.ssafy.test.dto.User;
 import com.ssafy.test.dto.UserInfo;
 
 public interface UserService {
 
 	UserInfo login(UserInfo userInfo) throws Exception;
+
+	void createKakaoUser(User kakaoUser);
+
+	User selectUser(User kakaoUser);
 
 }
--- spring-test/src/main/java/com/ssafy/test/service/UserServiceImpl.java
+++ spring-test/src/main/java/com/ssafy/test/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.ssafy.test.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ssafy.test.dto.User;
 import com.ssafy.test.dto.UserInfo;
 import com.ssafy.test.mapper.UserMapper;
 
@@ -15,5 +16,17 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public UserInfo login(UserInfo userInfo) throws Exception {
 		return mapper.selectLogin(userInfo);
+	}
+
+	@Override
+	public void createKakaoUser(User kakaoUser) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public User selectUser(User kakaoUser) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
